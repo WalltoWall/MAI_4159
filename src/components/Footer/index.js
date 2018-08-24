067923fbdeede81ac391a8cccd3582ec499abf92
Hyunwoo Kang <khwken@gmail.com>
2018-08-23 18:13:37 -1000

    implement footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,57 +1,82 @@
 import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
 import { Link } from 'components/Link'
-import styled from 'react-emotion'
-import { css } from 'emotion'
-import t from 'theme'
 import logo from 'assets/named_logo.svg'
+import { get } from 'lodash'
+import {
+  Container,
+  Content,
+  FooterLogo,
+  BrandContent,
+  MenuContainer,
+  LinkContainer,
+  StyledLink,
+  LinkSeparator,
+  CopyrightContent
+} from './index.styled'
+
+const render = () => queryData => {
+  const menuList = get(queryData, 'prismicNavigation.data.primary');
+  const menuLength = menuList.length;
+
+  return (
+    <Container>
+      <Content>
+        <Link to="/">
+          <FooterLogo src={logo} />
+        </Link>
+        <BrandContent>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
+        </BrandContent>
+        <MenuContainer>
+          <LinkContainer>
+            {menuList.map(
+              (item, index) => (
+              <>
+                <StyledLink to="/"
+                  key={get(item, 'id')}
+                  to={get(item, 'primary.link.url')} 
+                >
+                  {get(item, 'primary.name')}
+                </StyledLink>
+                {
+                  menuLength !== (index + 1) && 
+                  <LinkSeparator>|</LinkSeparator>
+                }
+              </>
+              )
+            )}
+          </LinkContainer>
+          <CopyrightContent>
+            © 2074 Sed ut porttitor purus. Quisque ligula lectus. Proin vitae diam ipsum.
+          </CopyrightContent>
+          </MenuContainer>
+      </Content>
+    </Container>
+  )
+}
 
 export const Footer = () => (
-  <Container>
-    <Content>
-    <LinkList>
-        <Link to="/">
-          <img src={logo} className={namedLogoClassName}/>
-        </Link>
-        <Link>
-          there
-        </Link>
-      </LinkList>
-      <LinkList>
-        <Link>
-          hi
-        </Link>
-        <Link>
-          there
-        </Link>
-        <Link>
-          I'm
-        </Link>
-        <Link>
-          footer
-        </Link>
-      </LinkList>
-    </Content>
-  </Container>
-)        
+  <StaticQuery
+    query={graphql`
+      query {
+        prismicNavigation {
+          data {
+            primary {
+              id
+              primary {
+                name
+                link {
+                  url
+                }
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={render()}
+  />
+)
 
-export const Container = styled.div`
-  background-color: ${t.c.darkgrey1};
-  padding: ${t.s(0)} * 0.9;
-  position: relative;
-`
-export const Content = styled.div`
-  margin: 0 auto;
-  max-width: ${t.ms.l};
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`
-export const LinkList = styled.ul`
-  
-`
 
-export const namedLogoClassName = css`
-  width: 40%;
-  margin-top: -1.9rem;
-  z-index: 3;
-`
