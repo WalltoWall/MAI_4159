476f03e22e1b8ae02bffc0803aee4547977c0d74
Hyunwoo Kang <khwken@gmail.com>
2018-08-24 16:15:19 -1000

    implement hero slice for portfolio page

--- src/slices/ProjectBodyHero/index.js
+++ src/slices/ProjectBodyHero/index.js
@@ -1,25 +1,30 @@
 import React from 'react'
 import { graphql } from 'gatsby'
-
-export const ProjectBodyHero = () => (
-  <div>
-      hello project
-  </div>
+import { get } from 'lodash'
+ import { Gradient, Container, ImageContainer, Image } from './index.styled'
+ export const ProjectBodyHero = ({ data }) => (
+    <Container>
+      <ImageContainer>
+        <Gradient />
+        <Image src={get(data, 'primary.image.url')} />
+      </ImageContainer>
+    </Container>
 )
-
-export const query = graphql`
+ export const query = graphql`
   fragment ProjectBodyHero on Query {
     prismicProject(id: { eq: $id }) {
       data {
         body {
           ... on PrismicProjectBodyHero {
             id
+            primary {
+              image {
+                url
+              }
+            }
           }
         }
       }
     }
   }
 `
-
-
-
