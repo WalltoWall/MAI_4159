b920e03a23361535214ffb7e53f3c03e6e153e38
Ken <khwken@gmail.com>
2018-08-28 17:37:54 -1000

    implement angled div overlay for portfolio page

--- src/slices/PageLayoutSubPageHero/index.js
+++ src/slices/PageLayoutSubPageHero/index.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import { get } from 'lodash'
-import { Gradient, Container, ImageContainer, Image } from './index.styled'
+import { Overlay, ImageContainer, Image } from './index.styled'
 export const PageLayoutSubPageHero = ({ data }) => (
-  <Container>
+  
     <ImageContainer>
-      <Gradient />
       <Image src={get(data, 'primary.image.url')} />
+      <Overlay />
     </ImageContainer>
-  </Container>
 )
  export const query = graphql`
   fragment PageLayoutSubPageHero on Query {
