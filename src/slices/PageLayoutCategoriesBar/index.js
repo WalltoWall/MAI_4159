93e6313b2241ee2dfb5e59cc6bfbac28d0e73586
Ken <khwken@gmail.com>
2018-08-31 15:19:41 -1000

    implement dropdown categories filter

--- src/slices/PageLayoutCategoriesBar/index.js
+++ src/slices/PageLayoutCategoriesBar/index.js
@@ -1,17 +1,55 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import { get } from 'lodash'
-import { Container, StyledLink } from './index.styled'
+import { Toggle } from 'react-powerplug'
+import { 
+  Desktop, 
+  Mobile, 
+  StyledLink,
+  FilterBox,
+  CurrentFilter,
+} from './index.styled'
 
-export const PageLayoutCategoriesBar = ({data}) => (
-  <Container>
-    {get(data, 'items', []).map(item => (
-      <StyledLink key={get(item, 'name.text')} to={get(item, 'url.url', '/')}   >
-        {get(item, 'name.text')}              
-      </StyledLink>
-    ))}
-  </Container>
-)
+export class PageLayoutCategoriesBar extends React.Component {
+  constructor(props) {
+    super(props)
+
+  }
+
+  componentDidMount() {
+    
+  }
+
+  render() {
+    const categories = get(this.props, 'data.items')
+    return (
+      <>
+        <Desktop>
+          {categories.map(item => (
+            <StyledLink key={get(item, 'name.text')} to={get(item, 'url.url', '/')}   >
+              {get(item, 'name.text')}              
+            </StyledLink>
+          ))}
+        </Desktop>
+        <Toggle>
+          {({on, toggle}) => 
+            <Mobile> 
+              <span>Filter: </span>
+              <CurrentFilter onClick={toggle}>active filter name</CurrentFilter>
+              <FilterBox isOpen={on}>
+                {categories.map(item => (
+                  <StyledLink key={get(item, 'name.text')} to={get(item, 'url.url', '/')}   >
+                    {get(item, 'name.text')}              
+                  </StyledLink>
+                ))}
+              </FilterBox>
+            </Mobile>
+          }
+        </Toggle>
+      </>
+    )
+  }
+}
 
 export const query = graphql`
   fragment PageLayoutCategoriesBar on Query {
--- src/slices/PageLayoutCategoriesBar/index.styled.js
+++ src/slices/PageLayoutCategoriesBar/index.styled.js
@@ -2,17 +2,50 @@ import styled from 'react-emotion'
 import { Link } from 'components/Link'
 import t from 'theme'
 
-export const Container = styled.div`
+export const Desktop = styled.div`
+    position: relative;
     background-color: ${t.c.darkgrey2};
-    display: flex;
+    display: none;
     justify-content: center;
     padding: ${t.s(1)} 0;
     color: ${t.c.white};
     font-size: ${t.f(1)};
     font-weight: 700;
+    z-index:2;
+    ${t.mq.l} {
+        display: flex;
+    }
  `
 
+export const Mobile = styled.div`
+    background-color: ${t.c.darkgrey2};
+    padding: ${t.s(1)} 0;
+    text-align: center;
+    ${t.mq.l} {
+        display: none;
+    }
+    span {
+     font-size: ${t.f(1)};
+     color: ${t.c.white};
+     font-weight: 600;    
+    }
+`
+export const FilterBox = styled.div`
+    display: ${p => p.isOpen ? 'flex' : 'none'};
+    flex-direction: column;
+    padding-top: 1rem;
+`
+
+export const CurrentFilter = styled.h2`
+    display: inline-block;
+    margin: 0;
+    font-size: ${t.f(1)};
+    color: ${t.c.yellow};
+    text-align: center; 
+`
+
 export const StyledLink = styled(Link)`
+    z-index: 2;
     padding: 0 1rem;
     -webkit-transition: color .2s ease-in;
     -moz-transition: color .2s ease-in;
