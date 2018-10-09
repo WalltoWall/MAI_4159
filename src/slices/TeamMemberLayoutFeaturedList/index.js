dad6aea584dce15341e45726d4690d1cab4b6c9a
Ken <khwken@gmail.com>
2018-10-09 12:20:45 -1000

    implement team member featured project list slice

--- src/slices/TeamMemberLayoutFeaturedList/index.js
+++ src/slices/TeamMemberLayoutFeaturedList/index.js
@@ -1,16 +1,78 @@
 import React from 'react'
 import { graphql } from 'gatsby'
+import { Container, GridContainer, ImageContainer, Content } from './index.styled'
+import { get } from 'lodash'
+import { Image } from 'components/Image'
+import { Link } from 'components/Link'
 
-export const TeamMemberLayoutFeaturedList = ({ data }) => (
-  <h1>hello feature list</h1>
-)
+export const TeamMemberLayoutFeaturedList = ({ data }) => {
+  console.log("sss", data)
+  return (
+  <Container>
+    {get(data, "items").map(item => (
+      <GridContainer>
+        <Link
+         to={get(item, "project.url")}
+        >
+          <ImageContainer>
+            <Image 
+              fluid={get(item, "project.document[0].data.image.localFile.childImageSharp.fluid")}
+            />          
+          </ImageContainer> 
+        </Link>
+        <Content>
+          <h2>
+            {get(item, "project.document[0].data.title.text")}  
+          </h2>          
+          <hr align="left"/>
+          <div>
+            <span>Project Type</span>
+            <p>{get(item, "project.document[0].data.project_type.text")}</p>
+          </div>
+          <hr align="left"/>
+          <div>
+            <span>Year Completed</span>
+            <p>{get(item, "project.document[0].data.year_completed")}</p>
+          </div>
+        </Content>
+      </GridContainer>      
+      
+    ))}    
+  </Container>
+  )
+}
 export const query = graphql`
   fragment TeamMemberLayoutFeaturedList on Query {
     prismicTeamMember(id: { eq: $id }) {
       data {
         layout {
           ... on PrismicTeamMemberLayoutFeaturedList {
-            id          
+            id
+            items {
+              project {
+                url
+                document {
+                  data {
+                    title {
+                      text
+                    }
+                    project_type {
+                      text
+                    }
+                    year_completed          
+                    image {
+                      localFile {
+                        childImageSharp {
+                          fluid(maxWidth: 800, quality: 90) {
+                            ...GatsbyImageSharpFluid_withWebp_noBase64
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }       
           }
         }
       }
