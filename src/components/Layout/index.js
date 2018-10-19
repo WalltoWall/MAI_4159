bb64d699ab257da259c955b8cc4b7680df828640
AlissaMasutani <alissa.masutani@walltowall.com>
2018-10-19 13:55:13 -1000

    get bugherd working

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -63,6 +63,16 @@ const render = ({ children }) => queryData => (
     >
       <html lang="en" />
       <Typekit kitId="air2qxr" />
+      <script>
+        {`
+          (function (d, t) {
+            var bh = d.createElement(t), s = d.getElementsByTagName(t)[0];
+            bh.type = 'text/javascript';
+            bh.src = 'https://www.bugherd.com/sidebarv2.js?apikey=wi4lcps15xlmmpaktwznxg';
+            s.parentNode.insertBefore(bh, s);
+            })(document, 'script');
+        `}
+      </script>
     </Helmet>
     <ModalProvider>
       <ModalConsumer>
