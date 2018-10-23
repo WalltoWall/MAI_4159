1c619298c5715b6f7f78bf39fe1de83af3ad503e
Ken <khwken@gmail.com>
2018-10-23 11:48:54 -1000

    fix search result count bug

--- src/components/SearchResults/index.js
+++ src/components/SearchResults/index.js
@@ -1,18 +1,32 @@
 import React from 'react'
-
+import {get, head, last} from 'lodash'
 import { Container, StyledHTMLContent, Title, Items } from './index.styled'
 import { Placeholder } from 'components/Placeholder'
 
-export const SearchResults = ({ children, query, queryType, ...props }) => (
-  <Container>
-    <StyledHTMLContent>
-      <Title>{queryType}</Title>
-      <p>
-        Showing {children.length} {queryType.slice(0, -1)} result
-        {children.length !== 1 && 's'} for <strong>{query}</strong>
-      </p>
-    </StyledHTMLContent>
-    <Items>{children}</Items>
-    <Placeholder />
-  </Container>
-)
+export const SearchResults = ({ children, query, queryType, ...props }) => {  
+  let resultLength
+  
+  if (queryType === "Projects") {
+    resultLength = children[0].props.children.length
+  } else {
+    resultLength = children[1].length
+  } 
+  
+  return (
+    <Container>
+      <StyledHTMLContent>
+        <Title>{queryType}</Title>
+        <p>
+          {"Showing" + " "}
+          {resultLength + " "} 
+          {queryType.slice(0, -1) + " "} 
+          result
+          {children.length !== 1 && 's'} for <strong>{query}</strong>
+        </p>
+      </StyledHTMLContent>
+      <Items>{children}</Items>
+      <Placeholder />
+    </Container>
+  )
+}
+  
