da29b2af52b3a7ee175cff1c802f117f452333a0
Ken <khwken@gmail.com>
2018-11-27 17:06:57 -1000

    change gatsby netlify plugin version

--- gatsby-config.js
+++ gatsby-config.js
@@ -396,7 +396,7 @@ module.exports = {
     },
     'gatsby-plugin-catch-links',
     'gatsby-transformer-sharp', 
-    'gatsby-plugin-netlify',  
+    'gatsby-plugin-netlify',
     'gatsby-plugin-netlify-cache',
   ],
 }
