0942f0af0af02629d4eeef4927c11ca908150685
Ken Kang <ken.kang@walltowall.com>
2019-01-30 09:57:23 -1000

    fix: fix minor typo

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -45,7 +45,7 @@ const render = () => queryData => {
             ))}
           </LinkContainer>
           <CopyrightContent>
-            © 2019 Mason
+            © 2019 MASON
             <br />
             Website designed and developed by{' '}
             <a href="https://www.walltowall.com" target="blank">
