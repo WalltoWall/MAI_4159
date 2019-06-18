2e720df2100f2d6351378313f80331b32b37a56c
ken hw kang <khwken@gmail.com>
2019-06-18 12:04:26 -1000

    fix: code cleanup

--- src/templates/project.js
+++ src/templates/project.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Helmet } from 'react-helmet'
 import MapToComponents from 'react-map-to-components'
 import { graphql } from 'gatsby'
